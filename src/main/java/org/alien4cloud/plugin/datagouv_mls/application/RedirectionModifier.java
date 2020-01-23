6d34561135a94ac735268f2df22219fe2670a493
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-01-23 22:09:26 +0100

    [ALIEN-3347] A sample modifier that triggers URL redirection

---
+++ src/main/java/org/alien4cloud/plugin/datagouv_mls/application/RedirectionModifier.java
@@ -0,0 +1,27 @@
+package org.alien4cloud.plugin.datagouv_mls.application;
+
+import alien4cloud.topology.task.RedirectionTask;
+import lombok.extern.slf4j.Slf4j;
+import org.alien4cloud.alm.deployment.configuration.flow.FlowExecutionContext;
+import org.alien4cloud.alm.deployment.configuration.flow.TopologyModifierSupport;
+import org.alien4cloud.tosca.model.templates.Topology;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component("datagouv_mls-redirection-sample")
+@Slf4j
+public class RedirectionModifier extends TopologyModifierSupport {
+
+    private Random random = new Random();
+
+    @Override
+    public void process(Topology topology, FlowExecutionContext context) {
+        // Let's randomly generate a DuckDuckGo URL searching images for a randomly generated number
+        String url = String.format("https://duckduckgo.com/?q=%d&iar=images&iax=images&ia=images", random.nextInt(50));
+        // This warn will be used in legacy A4C UI
+        context.log().warn("Please use the following URL to continue : " + url);
+        // This error will stop the deployment flow and trigger an URL redirection in Wizard
+        context.log().error(new RedirectionTask(url));
+    }
+}
