191daac68607a86c3cf0717625de54d614d36b39
A4C Team <centos@a4c-build1.a4c.net>
2021-01-06 16:47:44 +0100

    [ALIEN-3573] Display SMD errors as errors in UI

--- src/main/java/org/alien4cloud/plugin/datagouv_mls/application/DatagouvMLSModifier.java
+++ src/main/java/org/alien4cloud/plugin/datagouv_mls/application/DatagouvMLSModifier.java
@@ -334,7 +334,8 @@ public class DatagouvMLSModifier extends TopologyModifierSupport {
                     log.error("DataGouv response contains no entity !");
                     if (retAppli.getMessage() != null) {
                        log.error("Error while declaring application: " + retAppli.getMessage() + " [" + retAppli.getCode() + "]");
-                       context.log().info("Error while declaring application: " + retAppli.getMessage());
+                       context.log().error("Error while declaring application: " + retAppli.getMessage() +
+                                            (isSet(retAppli.getCode()) ? " [" + retAppli.getCode() + "]" : ""));
                     }
                 } else for (Entity retEntity : retAppli.getEntities()) {
                     if (retEntity.getTypeName().equals(DatagouvMLSConstants.MODULE_INSTANCE_NAME)) {
@@ -433,7 +434,8 @@ public class DatagouvMLSModifier extends TopologyModifierSupport {
                     log.error("DataGouv GetPds response contains no zone!");
                     if (pds.getMessage() != null) {
                        log.error("DataGouv GetPds error: " + pds.getMessage() + " [" + pds.getCode() + "]");
-                       context.log().info("Error while getting PDS: " + pds.getMessage());
+                       context.log().error("Error while getting PDS: " + pds.getMessage() +
+                                            (isSet(pds.getCode()) ? " [" + pds.getCode() + "]" : ""));
                     }
                 } else {
                     processPds(topology, context, pds, pds.getZone(), appliName);
